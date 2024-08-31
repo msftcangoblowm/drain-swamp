38cf42d20667ee33344cdc60fd7bde37df340ddf
msftcangoblowme <msftcangoblowme@protonmail.com>
2024-08-31 09:56:58 +0000

    requirement file suffix
    - ci(branch-test-others): fix requirement file suffix

--- src/drain_swamp/_version.py
+++ src/drain_swamp/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.6.3.dev0'
-__version_tuple__ = version_tuple = (1, 6, 3, "dev0", "gfbc1800.d20240831")
+__version__ = version = '1.6.3.dev1'
+__version_tuple__ = version_tuple = (1, 6, 3, "dev1", "g608d812.d20240831")
