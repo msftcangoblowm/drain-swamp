c6ad7cfbf29f4a369e54dd4d511923f58e11bdbc
msftcangoblowme <msftcangoblowme@protonmail.com>
2024-08-31 10:05:18 +0000

    build package before tox
    - ci(branch-test-others): build package before tox

--- src/drain_swamp/_version.py
+++ src/drain_swamp/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.6.3.dev1'
-__version_tuple__ = version_tuple = (1, 6, 3, "dev1", "g608d812.d20240831")
+__version__ = version = '1.6.3.dev2'
+__version_tuple__ = version_tuple = (1, 6, 3, "dev2", "g38cf42d.d20240831")
